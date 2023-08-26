6d9602f092f0cc57ac6ae6d56f12b3006580cea1
Brisstone <okolijohnson69@gmail.com>
2023-08-26 08:29:19 +0100

    OJ-resume-revamp

--- components/ProjectsNavbar.tsx
+++ components/ProjectsNavbar.tsx
@@ -24,6 +24,7 @@ const ProjectsNavbar: FunctionComponent<{
     <div className="flex px-3 py-2 space-x-3 overflow-x-auto list-none">
       <NavItem value="all" {...props} />
       <NavItem value="React" {...props} />
+      <NavItem value="Mobile" {...props} />
       {/* <NavItem value="Mongo" {...props} />
       <NavItem value="Django" {...props} /> */}
       <NavItem value="Node" {...props} />
--- components/ServiceCard.tsx
+++ components/ServiceCard.tsx
@@ -13,8 +13,8 @@ const ServiceCard: FunctionComponent<{ service: Service }> = ({
    }
 
    return (
-      <div className='flex items-center p-2 space-x-4 '>
-         <Icon className='w-12 h-12 text-green' />
+      <div className='p-2 space-x-4 '>
+         <Icon className='w-12 h-12 text-green mb-4 ml-3' />
          <div className=''>
             <h6 className='font-bold'>{title}</h6>
             <p dangerouslySetInnerHTML={createMarkup()} />
